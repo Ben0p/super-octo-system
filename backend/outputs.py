0ae650e3bf229e0769573b02a4541fa3d85c9f94
Ben0p <skittle7ben@msn.com>
2019-04-28 11:07:38 +0800

    set desired state

--- backend/outputs.py
+++ backend/outputs.py
@@ -1,7 +1,8 @@
 import pymongo
 from bson.json_util import dumps
 import json
-
+import time
+from pyModbusTCP.client import ModbusClient
 
 
 # Initialize mongo
@@ -9,46 +10,91 @@ client = pymongo.MongoClient('mongodb://10.1.1.94:27017/')
 db = client['super-octo-system']
 
 
+def get():
+    # Get list of modules
+    modules = db['modules'].find()
+
+    used_modules = []
+
+    # Find outputs in use for each module
+    for module in modules:
+        used_outputs = db['outputs'].find(
+            {
+                'parent' : module['parent']
+            }
+        )
+
+        output_states = []
+
+        for used_output in used_outputs:
+            output_states.append(
+                {
+                    'output' : used_output['output'],
+                    'desired' : used_output['set']
+                }
+            )
+
+        module_target = {
+            'ip' :  module['ip'],
+            'states' : output_states
+        }
+
+        used_modules.append(module_target)
+
+    return(used_modules)
+
 
 def read(ip):
-    
-    # Initialize 
+
+    # Initialize modbus
     c = ModbusClient(host=ip, port=502, auto_open=True, timeout=1)
 
-    try:
-        # Read the outputs again
-        bits = c.read_coils(16, 6)
+    # Read the outputs (Returns None if timeout)
+    return(c.read_coils(16, 6))
+
+def write(ip, output, state):
+    # Initialize modbus
+    c = ModbusClient(host=ip, port=502, auto_open=True, timeout=1)
+    coil = output+16
+    c.write_single_coil(coil, state)
+    print("Set output {} to {} on module ip {}".format(output, state, ip))
+    
 
 
-def trigger():
+def update(states):
+
+    for idx, state in enumerate(states):
+        db['outputs'].find_one_and_update(
+            {
+                'output' : idx
+            },
+            {
+                '$set': 
+                {
+                'state' : state
+                }
+            },
+            upsert = True
+        )
+
 
 
 
 def run():
-
-    # Get details from db
-    outputs = db['outputs'].find()
-    modules = db['modules'].find()
-
-    for module in modules:
-        # Get modules ip
-        ip = module['ip']
-
-        # Get current states
-        states = read(ip)
+    desired_states = get()
+    for module in desired_states:
+        if len(module['states']) > 0:
+            current_states = read(module['ip'])
+            for desired_state in module['states']:
+                if desired_state['desired'] != current_states[desired_state['output']]:
+                    write(module['ip'], desired_state['output'], desired_state['desired'])
 
 
-    # Check if set is different
-
-    # Change output to match set value
-
-    
-
-    for document in documents:
-        if document['set'] == True:
 
 
 
 
 if __name__ == '__main__':
-    run()
+    while True:
+        run()
+        time.sleep(0.5)
