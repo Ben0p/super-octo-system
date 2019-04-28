3e6d5a07d61131382bb2ca2df4516ba80b907277
Ben0p <skittle7ben@msn.com>
2019-04-28 12:25:35 +0800

    post output rest

--- backend/rest.py
+++ backend/rest.py
@@ -37,56 +37,47 @@ class outputs(Resource):
         # Return collection as a massive json
         return(jsonify(json.loads(dumps(io))))
 
-    def post(self, parent):
+    def post(self):
 
         # Blank output array
         outputs = []
 
         # Parse the form data
         parser = reqparse.RequestParser()
-        parser.add_argument("0")
-        parser.add_argument("1")
-        parser.add_argument("2")
-        parser.add_argument("3")
-        parser.add_argument("4")
-        parser.add_argument("5")
+        parser.add_argument('module')
+        parser.add_argument('output')
+        parser.add_argument('state')
 
         args = parser.parse_args()
 
         # False lookup
-        false_array = ['False', 'false', '0', False]
+        false_array = ['False', 'false', '0', False, None]
 
-        # Get alert details from database by ip
-        io_document = db['io-modules'].find_one({'parent': parent})
-        if io_document == None:
-            return(404)
+        if args['state'] in false_array:
+            state = False
+        else:
+            state = True
+
+        print("Module: {} | Output: {} | Set: {}".format(args['module'], args['output'], state))
+
+        db['outputs'].find_one_and_update(
+            {
+                'parent' : args['module'],
+                'output' : int(args['output'])
+            },
+            {
+                '$set': 
+                {
+                'set' : state
+                }
+            },
+            upsert = False
+        )
+        return(201)
 
 
-        ip = io_document['ip']
-        c = ModbusClient(host=ip, port=502, auto_open=True, timeout=1)
-        
-        outputs = [args['0'], args['1'], args['2'], args['3'], args['4'], args['5']]
 
-        for index, output in enumerate(outputs):
-            if output in false_array:
-                outputs[index] = ''
 
-        try:
-            # Set output states via modbus
-            c.write_single_coil(16, outputs[0])
-            c.write_single_coil(17, outputs[1])
-            c.write_single_coil(18, outputs[2])
-            c.write_single_coil(19, outputs[3])
-            c.write_single_coil(20, outputs[4])
-            c.write_single_coil(21, outputs[5])
-
-            # Read the outputs again
-            bits = c.read_coils(16, 6)
-        
-        except: 
-            return(404)
-        
-        return(bits)
 
 
 # Map URL's to resource classes
